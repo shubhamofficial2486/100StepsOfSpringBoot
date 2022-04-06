a0784957a4ea56f764b2c160d2e81f46f7ecbb56
shubhamofficial2486 <45324404+shubhamofficial2486@users.noreply.github.com>
2022-04-06 12:33:35 +0530

    Add files via upload

---
+++ jpa-in-10-steps/src/main/java/com/cognizant/jpa/jpain10steps/service/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cognizant.jpa.jpain10steps.service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognizant.jpa.jpain10steps.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
